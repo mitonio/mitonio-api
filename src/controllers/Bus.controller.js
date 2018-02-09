e2d0f8e8d90935be1ceb2a9d543b79df7b8d42de
Matheus Galdino <matheus.galdino@concrete.com.br>
2018-02-09 17:43:45 -0200

    Fixed bus line to search

--- src/controllers/Bus.controller.js
+++ src/controllers/Bus.controller.js
@@ -4,7 +4,7 @@ const apiKey = '5243fc248bc5f64a2f29f29b106e9b1b6a0985a837063ce76a12e4bea7a325a9
 const BusController = (busService = busLibrary) => ({
   async findBusLocation(req, res) {
     const { data, headers } = await busService.loadApi(apiKey);
-    const busLines = await busService.findBusByLine(8000, apiKey, headers['set-cookie']);
+    const busLines = await busService.findBusByLine(req.params.busNumber, apiKey, headers['set-cookie']);
     
     return res.send(busLines.data);
   },
@@ -14,6 +14,11 @@ const BusController = (busService = busLibrary) => ({
     const busLines = await busService.findStopsByBusLine(req.params.hall, apiKey, headers['set-cookie']);
     
     return res.send(busLines.data);
+  },
+
+  async findBusLocalization(req, res) {
+    const { data, headers } = await busService.loadApi(apiKey);
+    const busLines = await busService.findBusLocalization(req.params.busNumber, headers['set-cookie']);
   },
 });
 
