2203e6d654fd81511c9eb2d7b2d75e4dd9ed40ac
Matheus Galdino <matheus.galdino@hotmail.com>
2018-02-09 17:55:46 -0200

    Created middleware to extract token

--- src/routes/bus.routes.js
+++ src/routes/bus.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const busController = require('../controllers/Bus.controller');
+const busMiddleware = require('../middlewares/busMiddleware.middleware');
 
 const router = express.Router();
 
-router.get('/', busController.findBusLocation);
-router.get('/:hall', busController.findStopsByBusHall);
+router.get('/line/:busNumber', busController.findBusLocation);
+router.get('/stop/:busNumber', busController.findBusLocalization);
+router.get('/:hall', busMiddleware.getTokenAuth, busController.findStopsByBusHall);
 
 module.exports = router;
