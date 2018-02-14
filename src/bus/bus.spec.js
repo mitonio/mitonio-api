a9792a63e67c36051eb6aa2b3c189cbd1e147058
Matheus Galdino <matheus.galdino@hotmail.com>
2018-02-14 14:38:45 -0200

    Adding CI

--- src/bus/bus.spec.js
+++ src/bus/bus.spec.js
@@ -17,14 +17,14 @@ describe('Unit test: Bus Controller', () => {
     describe('busController.findBusByLine', () => {
       it('should return JSON with statusCode 200', async () => {
         const busLineRequest = await request
-          .get('192.168.33.10:3001/bus/line/8000');
+          .get('localhost:3001/bus/line/8000');
 
         expect(await busLineRequest.statusCode).to.be.equal(200);
       });
       
       it('should return JSON with data not null and error null', async () => {
         const busLineRequest = await request
-          .get('192.168.33.10:3001/bus/line/8000');
+          .get('localhost:3001/bus/line/8000');
         
         expect(await busLineRequest.body.data).to.not.be.empty;
         expect(await busLineRequest.body.error).to.be.null;
@@ -34,14 +34,14 @@ describe('Unit test: Bus Controller', () => {
     describe('busController.findStopsByBusLine', () => {
       it('should return JSON with statusCode 200', async () => {
         const busStopRequest = await request
-          .get('192.168.33.10:3001/bus/stop/8000');
+          .get('localhost:3001/bus/localization/8000');
 
         expect(await busStopRequest.statusCode).to.be.equal(200);
       });
 
       it('should return JSON with data not null and error null', async () => {
         const busStopRequest = await request
-          .get('192.168.33.10:3001/bus/stop/8000');
+          .get('localhost:3001/bus/localization/8000');
         
         expect(await busStopRequest.body.data).to.not.be.empty;
         expect(await busStopRequest.body.error).to.be.null;
