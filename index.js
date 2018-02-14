59a3c35e58d2df3e02566fd22ec6e31d2c07baa0
Matheus Galdino <matheus.galdino@hotmail.com>
2018-02-14 16:40:25 -0200

    Testing CI

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require('./src/app');
 const port = process.env.PORT || 8000;
 
 app.listen(port, (err) => {
-  if (err) console.log(`Error: ${err}`);
+  if (err) return `Error: ${err}`;
   console.log(`Running at port ${port}`);
 });
