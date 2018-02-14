20be3fc1c11b0d2b34441631d4aae35cee8b1c25
Matheus Galdino <matheus.galdino@hotmail.com>
2018-02-14 15:03:41 -0200

    Handling unrelated routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,4 +4,6 @@ const busRoutes = require('../bus/bus.routes');
 
 router.use('/bus', busRoutes);
 
+router.use((req, res) => res.status(404).json({ error: 'Not found' }));
+
 module.exports = router;
